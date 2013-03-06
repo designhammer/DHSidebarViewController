9fac482358a935f150c593aa0ed3d29728a864cc
Jay Roberts <jay@gloryfish.org>
2013-03-06 11:28:03 -0500

    Initial Commit

---
+++ DHSlidebarViewController/DHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DHAppDelegate.h
+//  DHSlidebarViewController
+//
+//  Created by Jay Roberts on 3/6/13.
+//  Copyright (c) 2013 DesignHammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DHSlidebarViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DHSlidebarViewController
+//
+//  Created by Jay Roberts on 3/6/13.
+//  Copyright (c) 2013 DesignHammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DHAppDelegate class]));
+    }
+}
